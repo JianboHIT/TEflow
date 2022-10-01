c352a9818860f38f73a0c8811c4576dcfe468704
jianbo_acer <zhjb1996@163.com>
2022-10-01 17:25:09 +0800

    Introduce support for polynomial inputs. (crucial)

--- src/TEoutput/engout.py
+++ src/TEoutput/engout.py
@@ -1,34 +1,116 @@
 import logging
-from abc import ABC, abstractmethod
-from pprint import pformat
-from scipy.integrate import cumtrapz, trapz
+from unittest import mock
 import numpy as np
+from pprint import pformat
+from abc import ABC, abstractmethod
+from scipy.integrate import cumtrapz, trapz
+from numpy.polynomial import Polynomial as Poly
+
+from .utils import AttrDict
 
 logger = logging.getLogger(__name__)
 
+def get_itgs(datas, mode='cum', Tc=None, Th=None):
+    mode = mode[0].lower()
+    if mode == 'p':
+        # p, poly, polynomial
+        if (Tc is None) or (Th is None) :
+            raise ValueError('Tc and Th are all required.')
+        return _get_itgs_poly(datas, Tc, Th)
+    elif mode in {'s', 'c'}:
+        # s, single
+        # c, cum, cumulative
+        return _get_itgs_interp(datas, mode)
+    else:
+        raise ValueError('Value of mode is invalid.')
+
+def _get_itgs_interp(datas_TCSK, mode='cum'):
+    T, C, S, K = datas_TCSK
+    Rho = 1E4 / C           # S/cm to uOhm.m
+    
+    mode = mode[0].lower()
+    if mode == 's':
+        Tc, Th = T[0], T[-1]
+        Sc, Sh = S[0], S[-1]
+        integral = trapz
+    elif mode == 'c':
+        Tc, Th = T[0], T[1:]
+        Sc, Sh = S[0], S[1:]
+        integral = cumtrapz
+    else:
+        raise ValueError('Value of mode is invalid.')
+    
+    itgs = AttrDict()
+    itgs['Tc']     = Tc
+    itgs['Th']     = Th
+    itgs['Sc']     = Sc
+    itgs['Sh']     = Sh
+    itgs['deltaT'] = Th - Tc
+    itgs['Rho']    = integral(Rho, T)         # <Rho>
+    itgs['S']      = integral(S, T)           # <S>
+    itgs['K']      = integral(K, T)           # <K>
+    itgs['RhoT']   = integral(T*Rho, T)       # <T*Rho>
+    itgs['ST']     = integral(T*S, T)         # <T*S>
+    return itgs
+
+def _get_itgs_poly(datas_RSK, Tc, Th):
+    Rho, S, K = datas_RSK
+    RhoT = Poly([0,1]) * Rho
+    ST   = Poly([0,1]) * Rho
+    
+    itgs = AttrDict()
+    itgs['Tc']     = Tc
+    itgs['Th']     = Th
+    itgs['Sc']     = S(Tc)
+    itgs['Sh']     = S(Th)
+    itgs['deltaT'] = Th - Tc
+    itgs['Rho']    = __poly_itg(Rho, Tc, Th)        # <Rho>
+    itgs['S']      = __poly_itg(S, Tc, Th)          # <S>
+    itgs['K']      = __poly_itg(K, Tc, Th)          # <K>
+    itgs['RhoT']   = __poly_itg(RhoT, Tc, Th)       # <T*Rho>
+    itgs['ST']     = __poly_itg(ST, Tc, Th)         # <T*S>
+    return itgs
+
+def __poly_itg(poly, a, b):
+    polyint = poly.integ()
+    return polyint(b) - polyint(a)
+
+def get_mdfs(itgs):
+    mdfs = AttrDict()
+    mdfs['Rho']  = itgs['RhoT'] / itgs['Rho']               # <T*Rho>/<Rho>
+    mdfs['S']    =   itgs['ST'] / itgs['S']                 # <T*S>/<S>
+    mdfs['RhoW'] = itgs['RhoT'] - itgs['Tc']*itgs['Rho']    # <(T-Tc)*Rho>, weighted Rho
+    mdfs['SW']   = 2*itgs['ST'] - itgs['Tc']*itgs['S']      # <(2T-Tc)*S>, weighted S
+    mdfs['RhoT'] = mdfs['Rho'] - itgs['Tc']       # <(T-Tc)*Rho>/<Rho>, weighted DT/2 by Rho
+    mdfs['ST']   = 2*mdfs['S'] - itgs['Tc']       # <(2T-Tc)*S>/<S>, weighted Th by S
+    mdfs['ST_RhoT_0'] = mdfs['ST']                          # modified Th
+    mdfs['ST_RhoT_1'] = mdfs['ST_RhoT_0'] - mdfs['RhoT']    # modified Tave
+    mdfs['ST_RhoT_2'] = mdfs['ST_RhoT_1'] - mdfs['RhoT']    # modified Tc
+    return mdfs
+
+def get_wgts(itgs, mdfs):    
+    Rho_w = mdfs['Rho']
+    TSc   = itgs['Tc'] * itgs['Sc']
+    TSh   = itgs['Th'] * itgs['Sh']
+    Tau_w = (TSh*itgs['deltaT'] - mdfs['SW']) / (TSh - TSc - itgs['S'])
+    
+    wgts = AttrDict()
+    wgts['W_J'] = Rho_w / itgs['deltaT']
+    wgts['W_T'] = Tau_w / itgs['deltaT']
+    wgts['alpha_0'] = mdfs['ST_RhoT_0'] / itgs['Sh']
+    wgts['alpha_1'] = mdfs['ST_RhoT_1'] / itgs['Sh']
+    wgts['alpha_2'] = mdfs['ST_RhoT_2'] / itgs['Sh']
+    return wgts
+
 class BaseDevice(ABC):
-    paras = dict()
-    options = dict()
-    profiles = dict()
-    outputs = dict()
-    _itgs = None
-    _mdfs = None
-    _wgts = None
+    paras = AttrDict()          # init
+    options = AttrDict()        # build-input
+    configs = AttrDict()        # simulate-input
+    profiles = AttrDict()       # build-output
+    outputs = AttrDict()        # simulate-output
     
     def __init__(self, **paras):
-        pass
-        
-    @abstractmethod
-    def build(self, **options):
-        pass
-    
-    @abstractmethod
-    def simulate(self, Jx='optimal'):
-        pass
-    
-    @classmethod
-    @abstractmethod
-    def valuate(cls, **kwargs):
+        self.paras.update(paras)
         pass
     
     def __getattr__(self, name):
@@ -40,97 +122,74 @@ class BaseDevice(ABC):
             return self.outputs[name]
         else:
             return super().__getattribute__(name)
+     
+    @abstractmethod
+    def build(self, **options):
+        self.options.update(options)
+        return self.profiles
+    
+    @abstractmethod
+    def simulate(self, **configs):
+        self.configs.update(configs)
+        return self.outputs
     
     @classmethod
-    def _get_itgs(cls, datas, isCum=True):
-        '''
-        Calculate integrals of TE properties.
-
-        Parameters
-        ----------
-        datas : list | ndarray
-            TE datas like [T, C, S, K]
-        isCum : bool, optional
-            Whether to cumulatively integrate using the composite trapezoidal rule (default: True).
-
-        Returns
-        -------
-        dict of ndarray
-            Sc, Sh, Tc, Th, deltaT, itg_Rho, itg_S, itg_K, itg_RhoT, itg_ST
-        '''
-        
-        T, C, S, K = datas
-        Rho = 1E4 / C                       # S/cm to uOhm.m
-        integral = cumtrapz if isCum else trapz
-        
-        itgs = dict()
-        itgs['_isCum']  = isCum
-        itgs['Sc']     = S[0]
-        itgs['Sh']     = S[1:] if isCum else S[-1]
-        itgs['Tc']     = T[0]
-        itgs['Th']     = T[1:] if isCum else T[-1]
-        itgs['deltaT'] = itgs['Th'] - itgs['Tc']
-        itgs['Rho']    = integral(Rho, T)         # <Rho>
-        itgs['S']      = integral(S, T)           # <S>
-        itgs['K']      = integral(K, T)           # <K>
-        itgs['RhoT']   = integral(T*Rho, T)       # <T*Rho>
-        itgs['ST']     = integral(T*S, T)         # <T*S>
-        cls._itgs = itgs
-        return itgs
+    @abstractmethod
+    def valuate(cls, **kwargs):
+        pass
     
-    @classmethod
-    def _get_mdfs(cls, itgs):
-        mdfs = dict()
-        mdfs['_isCum'] = itgs['_isCum']
-        mdfs['Rho']  = itgs['RhoT'] / itgs['Rho']   # <T*Rho>/<Rho>
-        mdfs['S']  = itgs['ST'] / itgs['S']         # <T*S>/<S>
-        mdfs['RhoW'] = itgs['RhoT'] - itgs['Tc']*itgs['Rho']  # <(T-Tc)*Rho>, weighted Rho
-        mdfs['SW'] = 2*itgs['ST'] - itgs['Tc']*itgs['S']      # <(2T-Tc)*S>, weighted S
-        mdfs['RhoT'] = mdfs['Rho'] - itgs['Tc']     # <(T-Tc)*Rho>/<Rho>, weighted DT/2 by Rho
-        mdfs['ST'] = 2*mdfs['S'] - itgs['Tc']       # <(2T-Tc)*S>/<S>, weighted Th by S
-        mdfs['ST_RhoT_0'] = mdfs['ST']          # modified Th
-        mdfs['ST_RhoT_1'] = mdfs['ST_RhoT_0'] - mdfs['RhoT']    # modified Tave
-        mdfs['ST_RhoT_2'] = mdfs['ST_RhoT_1'] - mdfs['RhoT']    # modified Tc
-        cls._mdfs = mdfs
-        return mdfs
-    
-    @classmethod
-    def _get_wgts(cls, itgs, mdfs):
-        if itgs['_isCum'] != mdfs['_isCum']:
-            dsp = 'IsCums of itgs ({}) and mdfs ({}) are incompatible.'
-            raise RuntimeError(dsp.format(itgs['_isCum'], mdfs['_isCum']))
-        
-        Rho_w = mdfs['Rho']
-        TSc = itgs['Tc'] * itgs['Sc']
-        TSh = itgs['Th'] * itgs['Sh']
-        Tau_w = (TSh*itgs['deltaT'] - mdfs['SW']) / (TSh - TSc - itgs['S'])
-        
-        wgts = dict()
-        wgts['_isCum'] = itgs['_isCum']
-        wgts['W_J'] = Rho_w / itgs['deltaT']
-        wgts['W_T'] = Tau_w / itgs['deltaT']
-        wgts['alpha_0'] = mdfs['ST_RhoT_0'] / itgs['Sh']
-        wgts['alpha_1'] = mdfs['ST_RhoT_1'] / itgs['Sh']
-        wgts['alpha_2'] = mdfs['ST_RhoT_2'] / itgs['Sh']
-        cls._wgts = wgts
-        return wgts
- 
 
 class Generator(BaseDevice):
     paras = {
+        'mode': 'cum',          # mode, cum | single | poly
         'TEdatas': None,        # TE datas
+        'Tc': None,
+        'Th': None,
         'L': 1,                 # length of TE leg
     }
     options = {
-        'isCum': True,          # calculate only at the T_max (False) or all-T (True, default)
         'calWeights': False,    # whether to calculate dimensionless weight factors of Joule and Thomson heat
         'returnProfiles': False,
     }
+    configs = {
+        'Jd_r': 'optimal',
+        'numPoints': 101,
+        'returnOutputs': False,
+    }
+    
     def __init__(self, **paras):
         logger.info('Begin initialization of {} ...'.format(self.__class__.__name__))
+        
+        mode = paras.get('mode', self.paras['mode'])
+        mode_short = mode[0].lower()
+        if mode_short not in {'c', 's', 'p'}:
+            raise ValueError('Value of mode is invalid.')
+        else:
+            logger.info('Mode of data: {}'.format(self.paras['mode']))
+        
+        TEdatas = paras['TEdatas']
+        if mode_short == 'p':
+            Rho, S, K = TEdatas
+            check = lambda x: isinstance(x, Poly)
+            if not all(map(check, [Rho, S, K])):
+                raise ValueError('TEdatas requires three numpy.polynomial.Polynomial.')
+            else:
+                logger.info('Read datas of TE properties in polynomial')
+                logger.debug('Value of TEdatas:\n%s', pformat(TEdatas))
+            
+            if {'Tc', 'Th'} <= paras:
+                Tc, Th = paras['Tc'], paras['Th']
+                logger.info('Tc is {}, and Th is {}'.format(Tc, Th))
+            else:
+                raise ValueError('Tc and Th are all required under polynomial properties.')
+        else:
+            T, C, S, K = TEdatas
+            datas = np.vstack([T,C,S,K]).T
+            logger.info('Read datas of TE properties')
+            logger.debug('Value of TEdatas:\n%s', pformat(datas))
+        
         self.paras.update(paras)
-        logger.info('Read parameters of device')
-        logger.debug('Parameters:\n%s', pformat(self.paras))
+        logger.info('Length of TE leg: {} mm'.format(self.paras['L']))
         logger.info('Finish initialization')
 
     def build(self, **options):
@@ -140,13 +199,14 @@ class Generator(BaseDevice):
         logger.info('Read options')
         logger.debug('Options:\n%s', pformat(options))
         
-        L = self.paras['L']
-        datas = self.paras['TEdatas']
-        
-        itgs = self._get_itgs(datas, isCum=options['isCum'])
-        mdfs = self._get_mdfs(itgs)
+        itgs = get_itgs(datas=self.paras['TEdatas'], 
+                        mode=self.paras['mode'],
+                        Tc=self.paras['Tc'],
+                        Th=self.paras['Th'])
+        mdfs = get_mdfs(itgs)
         logger.info('Calculate integrals and modification factors')
         
+        L = self.paras['L']
         deltaT = itgs['deltaT']
         PFeng = 1E-6 * itgs['S']*itgs['S']/itgs['Rho']
         Zeng = PFeng / itgs['K']
@@ -157,39 +217,46 @@ class Generator(BaseDevice):
         Jsc = 1E-1 * deltaT*itgs['S']/(itgs['Rho']*L)   # A/cm^2
         Qx = 0.1 * PFeng*deltaT/L      # W/cm^2, Qflux
         
-        self.profiles = {
-            '_isCum': options['isCum'],
-            'deltaT': deltaT,
-            'PFeng': PFeng,
-            'ZTeng': ZTeng,
-            'Zeng': Zeng,
-            'ZTp': ZTp,
-            'm_opt': m_opt,
-            'Voc': Voc,
-            'Jsc': Jsc,
-            'Qx': Qx,
-        }
+        profiles = AttrDict()
+        profiles['deltaT'] = deltaT
+        profiles['PFeng']  = PFeng
+        profiles['ZTeng']  = ZTeng
+        profiles['Zeng']   = Zeng
+        profiles['ZTp']    = ZTp
+        profiles['m_opt']  = m_opt
+        profiles['Voc']    = Voc
+        profiles['Jsc']    = Jsc
+        profiles['Qx']     = Qx
+        profiles['itgs']   = itgs
+        profiles['mdfs']   = mdfs
         logger.info('Calculate profiles of device')
         logger.debug('Keys of profiles:\n%s', pformat(self.profiles.keys()))
         
         if options['calWeights']:
-            wgts = self._get_wgts(itgs, mdfs)
-            self.profiles.update(wgts)
+            wgts = get_wgts(itgs, mdfs)
+            profiles.update(wgts)
             logger.info('Calculate weight factors')
         else:
             logger.debug('Ingore calculation of weight factors')
         
-        logger.info('Finish building process')
+        self.profiles = profiles
         if options['returnProfiles']:
-            return self.profiles
+            return profiles
+        logger.info('Finish building process')
         
-    def simulate(self, Jd_r='optimal', numPoints=101, returnOutputs=False):
+    def simulate(self, **configs):
         logger.info('Begin simulating ...')
+        self.configs.update(configs)
+        configs = self.configs
+        
+        # Jd_r='optimal', numPoints=101, returnOutputs=False
+        Jd_r = configs['Jd_r']
         if isinstance(Jd_r, str):
             if Jd_r.lower().startswith('o'):
                 Jd_r = None
                 logger.info('Work under optimial current density')
             elif Jd_r.lower().startswith('s'):
+                numPoints = configs['numPoints']
                 Jd_r = np.linspace(0, 1, numPoints)
                 logger.info('Work under auto-limition of current density')
         else:
@@ -198,17 +265,19 @@ class Generator(BaseDevice):
         
         deltaT = self.profiles['deltaT']
         Qx = self.profiles['Qx']
-        mdfs = self._mdfs
+        mdfs = self.profiles['mdfs']
         logger.info('Read out deltaT, Qx, and mdfs')
         
-        outputs = dict()
+        outputs = AttrDict()
         if Jd_r is None:
             m_opt = self.profiles['m_opt']
+            ST_RhoT_0 = mdfs['ST_RhoT_0']
+            ST_RhoT_2 = mdfs['ST_RhoT_2']
             outputs['Pd'] = 1/4 * Qx     # W/cm^2
-            outputs['Yita'] = 100 * deltaT * (m_opt-1)/(mdfs['ST_RhoT_0']*m_opt+mdfs['ST_RhoT_2'])
+            outputs['Yita'] = 100 * deltaT * (m_opt-1)/(ST_RhoT_0*m_opt+ST_RhoT_2)
             logger.info('Calculate Pd and Yita')
         else:
-            if self.profiles['_isCum']:
+            if self.paras['mode'][0].lower() == 'c':
                 Jd_r = np.reshape(Jd_r, (-1,1))
                 logger.debug('Reshape Jd_r to (-1,1)')
             Vout_r = 1-Jd_r
@@ -217,20 +286,20 @@ class Generator(BaseDevice):
             outputs['Jd'] = self.profiles['Jsc'] * Jd_r
             outputs['Vout'] = self.profiles['Voc'] * Vout_r
             outputs['Pd'] = Qx * Pd_r
-            outputs['Qhot'] = Qx/deltaT * Qhot_rt
-            outputs['Yita'] = 100 * deltaT * Pd_r / Qhot_rt
+            outputs['Qhot'] = Qx * Qhot_rt/deltaT
+            outputs['Yita'] = 100 * outputs['Pd'] / outputs['Qhot']
             logger.info('Calculate Jd, Vout, Pd, Qhot, and Yita')
         
         self.outputs = outputs
-        logger.info('Finish simulating process')
-        if returnOutputs:
+        if configs['returnOutputs']:
             return outputs
+        logger.info('Finish simulating process')
     
     @classmethod
-    def valuate(cls, datas, L=1):
-        gen = cls(TEdatas=datas, L=L)
+    def valuate(cls, datas_TCSK, L=1):
+        gen = cls(TEdatas=datas_TCSK, L=L)
         gen.build()
-        gen.simulate()
-        return gen.deltaT, gen.PFeng, gen.ZTeng, gen.Pd, gen.Yita
+        rst = gen.simulate(returnOutputs=True)
+        return rst.deltaT, rst.PFeng, rst.ZTeng, rst.Pd, rst.Yita
         
  
