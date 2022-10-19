b3ba45d94dc03c3bf3f6f0fd1686ba51f340eee3
jianbo_acer <zhjb1996@163.com>
2022-10-19 13:29:19 +0800

    update script/valuate_engout_gen.py

--- scripts/valuate_engout_gen.py
+++ scripts/valuate_engout_gen.py
@@ -20,21 +20,22 @@ logger = get_root_logger()
 logger.setLevel(LEVELNUM)
 
 sname = '{} - {}'.format(logger.name, os.path.basename(__file__))
-logger.info('Calcuate engineering performance of generator: {}'.format(sname))
+logger.info('Calcuate engineering performance of generator: %s', sname)
 
 # read input datas
 datas = np.loadtxt(filedatas, unpack=True, ndmin=2)
-logger.info('Read property datas from file {}'.format(filedatas))
+logger.info('Read property datas from file %s', filedatas)
 data_spot = pformat(datas[:,::10].T)
-logger.debug('Data spot checking:\n{}'.format(data_spot))
+logger.debug('Data spot checking:\n%s', data_spot)
 
 # perform calculation
 logger.info('Perform simulating of thermoelectric generator.')
-out = GenElementCore.valuate(datas, L=height)  # PFeng, ZTeng, Pd, Yita
-outdata = np.vstack(out).T
-logger.info('Export results: deltaT, PFeng, ZTeng, Pd, Yita')
+out = GenElementCore.valuate(datas, L=height)
+props = ['deltaT', 'PFeng', 'ZTeng', 'Pout', 'Yita']
+outdata = np.vstack([out[prop] for prop in props]).T
+logger.info('Export results: %s', ', '.join(props))
 data_spot = pformat(outdata[::10,:])
-logger.debug('Data spot checking:\n{}'.format(data_spot))
+logger.debug('Data spot checking:\n%s', data_spot)
 
 # output result
 if comment == '':
@@ -42,9 +43,10 @@ if comment == '':
     date = datetime.now().strftime("%Y.%m.%d")
     dsp = 'engineering output of generator with height {} mm ({})'
     comment = dsp.format(height, date)
-    logger.debug('Automatically generate comment:\n{}'.format(comment))
+    logger.debug('Automatically generate comment:\n%s', comment)
 else:
-    logger.debug('Read comment info.\n{}'.format(comment))
+    logger.debug('Read comment info.\n%s', comment)
 comment += '\ndeltaT[K] PFeng[W/m.K] ZTeng[1] Pd[W/cm^2] Yita[%]'
 np.savetxt(fileoutput, outdata, fmt='%10.4f', header=comment)
-logger.info('Save interpolation results to {}. (Done)'.format(fileoutput))
+logger.info('Save results to %s. (Done)', fileoutput)
+
