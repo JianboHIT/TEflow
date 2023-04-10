4c7ab86a2dd1f43ae81d0c313946218e459af6c2
lenovo02 <zhjb1996@163.com>
2023-04-10 10:41:58 +0800

    [NEW bandlib] develop bandlib.py submodule
    Migrate TEbands repository to here and rename to bandlib. It won't
    function properly until fully integrated [MERGE bandlib] into
    the main repo. Commit history is preserved solely for potential bug tracing.
    
    develop:
    1. BaseBand(), MultiBand() in bandlib/core.py
    2. vectorize() and constants kB, m_e, hbar, e0 in bandlib/utils.py

---
+++ src/teflow/bandlib/core.py
@@ -0,0 +1,171 @@
+from abc import ABC, abstractmethod
+from scipy.integrate import quad
+import numpy as np
+
+from .utils import kB, e0
+
+
+class BaseBand(ABC):
+    _caching = None
+    
+    @abstractmethod
+    def dos(self, E):
+        '''SI: 1E19 state/(eV.cm^3)'''
+        pass
+    
+    @abstractmethod
+    def trs(self, E, T):
+        '''SI: S/cm'''
+        pass
+    
+    def __K_n(self, __n, EF, T):
+        '''S/cm'''
+        # x = E/(kB*T),  E = x*kB*T
+        kernel = lambda x: self.trs(x*kB*T, T) * self.dfx(x - EF/(kB*T), __n)
+        return quad(kernel, 0, np.inf)[0]
+    
+    def K_n(self, __n, EF, T):
+        '''S/cm'''
+        if self._caching:
+            return self._caching['K_n'][__n]
+        else:
+            return np.vectorize(self.__K_n, excluded=['__n', ])(__n, EF, T)
+    
+    def __N(self, EF, T):
+        '''1E19 cm^(-3)'''
+        # x = E/(kB*T),  E = x*kB*T
+        kernel = lambda E: self.dos(E) * self.fx((E-EF)/(kB*T))
+        return quad(kernel, 0, np.inf)[0]
+    
+    def N(self, EF, T):
+        '''1E19 cm^(-3)'''
+        if self._caching:
+            return self._caching['N']
+        else:
+            return np.vectorize(self.__N)(EF, T)
+    
+    def compile(self, EF, T, max_level=2):
+        self.clear()
+        self._caching = {
+            'args': (EF, T), 
+            'K_n': [self.K_n(i, EF, T) for i in range(max_level+1)],
+            'N': self.N(EF, T),
+            'property': ('N', 'C', 'CS', 'S', 'PF', 'L', 'Ke', 'U'),
+        }
+    
+    def clear(self):
+        self._caching = None
+    
+    def __getattribute__(self, attr):
+        caching = super().__getattribute__('_caching')
+        if caching and (attr in caching['property']):
+            return super().__getattribute__(attr)(*caching['args'])
+        else:
+            return super().__getattribute__(attr)
+    
+    def C(self, EF, T):
+        '''S/cm'''
+        p0 = self.K_n(0, EF, T)
+        return p0
+    
+    def CS(self, EF, T):
+        '''[S/cm]*[uV/K]'''
+        p1 = self.K_n(1, EF, T)
+        return 1E6 * kB * p1
+    
+    def S(self, EF, T):
+        '''uV/K'''
+        p0 = self.K_n(0, EF, T)
+        p1 = self.K_n(1, EF, T)
+        return 1E6 * kB * p1/p0
+    
+    def PF(self, EF, T):
+        '''uW/(cm.K^2)'''
+        p0 = self.K_n(0, EF, T)
+        p1 = self.K_n(1, EF, T)
+        pr = np.power(p1, 2) / p0
+        return 1E6 * kB * kB * pr
+    
+    def L(self, EF, T):
+        '''1E-8 W.Ohm/K^2'''
+        p0 = self.K_n(0, EF, T)
+        p1 = self.K_n(1, EF, T)
+        p2 = self.K_n(2, EF, T)
+        pr = p2/p0 - np.power(p1/p0, 2)
+        return 1E8 * kB * kB * pr
+    
+    def Ke(self, EF, T):
+        '''W/(m.K)'''
+        p0 = self.K_n(0, EF, T)
+        p1 = self.K_n(1, EF, T)
+        p2 = self.K_n(2, EF, T)
+        pr = p2 - np.power(p1, 2)/p0
+        return 1E2 * kB * kB * pr * T
+    
+    def U(self, EF, T):
+        '''cm^2/(V.s)'''
+        pC = self.K_n(0, EF, T)     # S/cm
+        pN = self.N(EF, T)          # 1E19 cm^-3
+        return pC/(e0*pN*1E19)
+    
+    def rH(self, EF, T):
+        raise NotImplementedError('Undefined behaviour')
+    
+    @staticmethod
+    def fx(x):
+        return 1/2*(1-np.tanh(x/2))
+    
+    @staticmethod
+    def dfx(x, k=0):
+        return np.power(x, k) * 1/4*(1-np.power(np.tanh(x/2), 2))
+
+
+class MultiBand(BaseBand):
+    def __init__(self, bands=(), offsets=(), bands2=(), offsets2=()):
+        dsp = 'Length of {} is not the same as the number of {}'
+        if len(bands) != len(offsets):
+            raise ValueError(dsp.format('bands', 'offsets'))
+        if len(bands2) != len(offsets2):
+            raise ValueError(dsp.format('bands2', 'offsets2'))
+        self._bands = bands
+        self._bands2 = bands2
+        self._offsets = offsets
+        self._offsets2 = offsets2
+    
+    def dos(self, E):
+        dos_tot = 0
+        for bd, dt in zip(self._bands, self._offsets):
+            dos_tot += bd.dos(np.maximum(E-dt, 0))
+        for bd, dt in zip(self._bands2, self._offsets2):
+            dos_tot += bd.dos(np.maximum(dt-E, 0))
+        return dos_tot
+    
+    def trs(self, E, T):
+        trs_tot = 0
+        for bd, dt in zip(self._bands, self._offsets):
+            trs_tot += bd.trs(np.maximum(E-dt, 0), T)
+        for bd, dt in zip(self._bands2, self._offsets2):
+            trs_tot += bd.trs(np.maximum(dt-E, 0), T)
+        return trs_tot
+    
+    def K_n(self, __n, EF, T):
+        if self._caching:
+            K_tot = self._caching['K_n'][__n]
+        else:
+            K_tot = 0
+            for bd, dt in zip(self._bands, self._offsets):
+                K_tot += bd.K_n(__n, EF-dt, T)
+            for bd, dt in zip(self._bands2, self._offsets2):
+                K_tot += np.power(-1, __n) * bd.K_n(__n, dt-EF, T)
+        return K_tot
+    
+    def N(self, EF, T):
+        if self._caching:
+            N_tot = self._caching['N']
+        else:
+            N_tot = 0
+            for bd, dt in zip(self._bands, self._offsets):
+                N_tot += bd.N(EF-dt, T)
+            for bd, dt in zip(self._bands2, self._offsets2):
+                N_tot += bd.N(dt-EF, T)
+        return N_tot
---
+++ src/teflow/bandlib/utils.py
@@ -0,0 +1,40 @@
+from functools import wraps, partial
+import numpy as np
+
+
+kB = 8.617333262145179e-05      # eV/K
+m_e = 9.1093837015e-31          # kg
+hbar = 1.054571817e-34          # J.s
+e0 = 1.602176634e-19            # C
+
+def vectorize(__pyfunc=None, **kwargs):
+    '''
+    A wraper of numpy.vectorize 
+
+    Parameters
+    ----------
+    __pyfunc : callable, optional
+        A python callable object
+
+    Returns
+    -------
+    A wraped numpy.vectorize
+    
+    Example 1:
+    @vectorize
+    def func(...):
+        ...
+    
+    Example 2:
+    @vectorize(otype=...)
+    def func(...):
+        ...
+        
+    Example 2:
+    func = vectorize(func, ...)
+    '''
+    if __pyfunc is None:
+        return partial(vectorize, **kwargs)
+    else:
+        return wraps(__pyfunc)(np.vectorize(__pyfunc, **kwargs))
+
