9210fdec1ff08896cc6f44bb89f6428edc20eadf
jianbo_acer <zhjb1996@163.com>
2023-09-04 02:37:56 +0800

    develop UNIT dict in unils.py

--- src/teflow/bandlib/utils.py
+++ src/teflow/bandlib/utils.py
@@ -7,6 +7,23 @@ m_e = 9.1093837015e-31          # kg
 hbar = 1.054571817e-34          # J.s
 e0 = 1.602176634e-19            # C
 
+UNIT = {
+    'T': 'K',
+    'E': 'eV',
+    'N': '1E19 cm^(-3)',
+    'U': 'cm^2/(V.s)',
+    'C': 'S/cm',
+    'S': 'uV/K',
+    'K': 'W/(m.K)',
+    'L': '1E-8 W.Ohm/K^2',
+    'PF': 'uW/(cm.K^2)',
+    'ZT': '1',
+    'RH': 'cm^3/C',
+    'DOS': '1E19 state/(eV.cm^3)',
+    'TRS': 'S/cm',
+    'HALL': 'S.cm/(V.s)',
+}
+
 def vectorize(__pyfunc=None, **kwargs):
     '''
     A wraper of numpy.vectorize 
