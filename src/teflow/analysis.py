d48a5995a6fd4dccdcfd1341f99d15d223a7083f
jianbo_acer <zhjb1996@163.com>
2023-10-02 21:46:15 +0800

    introduce vquad() function in analysis.py

--- src/teflow/analysis.py
+++ src/teflow/analysis.py
@@ -15,6 +15,7 @@
 import numpy as np
 from numpy.polynomial import Polynomial as Poly
 from scipy.interpolate import interp1d
+from scipy.integrate import quad
 
 
 def interp(x, y, x2, method='linear', axis=-1, bounds_error=None, fill_value='extrapolate', merge=False):
@@ -248,5 +249,74 @@ def smoothstep(x, inverse=False, shift=True):
             x = np.minimum(np.maximum(x, 0), 1)
             return x*x*(3-2*x)
 
+def vquad(func, a, b, args=(), *, where=True, fill_value=0, **kwargs):
+    '''
+    Extend `scipy.integrate.quad` by adding support for broadcasting over
+    `a`, `b`, `args`, `where`, and `fill_value`. The `where` and `fill_value`
+    parameters are introduced for enhanced flexibility in controlling
+    integration.
 
+    Note that this function is not a true ufunc in the numpy sense. It
+    leverages broadcasting and Python loops to offer a convenient interface
+    for integrating a function across varying ranges and with diverse
+    parameters. It prioritizes ease of use over optimal performance.
 
+    Parameters
+    ----------
+    func : callable
+        A Python function or method to integrate.
+    a : array_like
+        Lower limit of integration.
+    b : array_like
+        Upper limit of integration.
+    args : tuple of array_like, optional
+        Extra arguments to pass to `func`. Each element of `args` will be
+        broadcasted to match the shape of `a` and `b`. Default is an empty
+        tuple.
+    where : array_like, optional
+        Boolean mask to specify where to perform the integration. Default
+        is `True`, which means that the integration is performed everywhere.
+    fill_value : array_like, optional
+        The value to use for masked positions. Default is `0`.
+    **kwargs
+        Additional keyword arguments passed to `scipy.integrate.quad`.
+
+    Returns
+    -------
+    ndarray
+        Array of computed integral values with the same shape as the
+        broadcasted shape of `a`, `b`, and each element of `args`.
+    ndarray
+        Array of estimated integration errors with the same shape as
+        the first returned ndarray.
+
+    Examples
+    --------
+    Consider a function `func(x, m, n)` defined as:
+
+    >>> def func(x, m, n):
+    ...     return m*x**2 if x<0 else n*x**3
+
+    We can integrate `func` over the intervals `[-1, 1]` and `[-2, 2]` with
+    `args` as `(3, [[4], [8], [12]])` . Here, `3` corresponds to 
+    the parameter `m` in `func`, and the column vector `[[4], [8], [12]]` 
+    corresponds to the parameter `n` in `func`. The broadcasting results in 
+    an output shape of `(3, 2)`:
+
+    >>> vquad(func, [-1, -2], [1, 2], args=(3, [[4], [8], [12]]))[0]
+    array([[ 2., 24.],
+           [ 3., 40.],
+           [ 4., 56.]])
+    '''
+    broadcasted = np.broadcast(a, b, where, fill_value, *args)
+    bshape = broadcasted.shape
+    itg = np.empty(bshape)
+    res = np.empty(bshape)
+    indexed = np.ndindex(bshape)
+
+    for idx, (ia, ib, unmask, default, *iargs) in zip(indexed, broadcasted):
+        if unmask:
+            itg[idx], res[idx] = quad(func, ia, ib, args=tuple(iargs), **kwargs)
+        else:
+            itg[idx], res[idx] = default, 0
+    return itg, res
