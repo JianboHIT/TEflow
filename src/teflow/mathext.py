5c9178b1bae04d431d917549723828c89b0e092f
jianbo_acer <zhjb1996@163.com>
2024-02-08 04:26:46 +0800

    remove mathext.interp() and interp1d of scipy

--- src/teflow/mathext.py
+++ src/teflow/mathext.py
@@ -15,62 +15,10 @@
 import numpy as np
 from numpy.polynomial import Polynomial as Poly
 from scipy.special import expit, logit
-from scipy.interpolate import interp1d
-from scipy.interpolate import CubicSpline, PchipInterpolator, Akima1DInterpolator, make_interp_spline
+from scipy.interpolate import CubicSpline, PchipInterpolator
+from scipy.interpolate import Akima1DInterpolator, make_interp_spline
 from scipy.integrate import quad
 
-
-def interp(x, y, x2, method='linear', axis=-1, bounds_error=None, fill_value='extrapolate', merge=False):
-    '''
-    A convenient method to implement scipy.interpolate.interp1d and Polynomial.fit.
-
-    Parameters
-    ----------
-    x : (N,) array_like
-        A 1-D array of real values.
-    y : (…,N) array_like
-        A N-D array of real values. 
-    x2 : array_like
-        The sampling points to evaluate the interpolated values.
-    method : str, optional
-        'linear'(default), 'nearest', 'nearest-up', 'zero', 'slinear', 'quadratic', 'cubic', 
-        'previous', 'next', 'poly1', 'poly2','poly3','poly4','poly5'.
-    axis : int, optional
-        Specifies the axis of y along which to interpolate, by default -1.
-    bounds_error : bool, optional
-        If True, a ValueError is raised any time interpolation is attempted on a value outside 
-        of the range of x (where extrapolation is necessary). If False, out of bounds values are 
-        assigned fill_value. By default, an error is raised unless fill_value="extrapolate".
-    fill_value : array-like or (array-like, array_like) or “extrapolate”, optional
-        A ndarray (or float) or a two-element tuple is supported. If “extrapolate”, then points 
-        outside the data range will be extrapolated.
-    merge : bool, optional
-        Whether to merge the sampling points into the interpolation result, by default False.
-
-    Returns
-    -------
-    datas: ndarray
-        The interpolated values, same shape as x.
-    '''
-    
-    if method.lower().startswith('poly'):
-        # polyfit
-        order = int(method[-1])
-        y2 = [x2,] if merge else []
-        for yi in y:
-            pfit = Poly.fit(x, yi, deg=order)
-            y2.append(pfit(x2))
-        datas = np.vstack(y2)
-    else:
-        # piecewise interpolate
-        fx = interp1d(x, y, 
-                      kind=method, 
-                      axis=axis, 
-                      bounds_error=bounds_error, 
-                      fill_value=fill_value)
-        y2 = fx(x2)
-        datas = np.vstack([x2,y2]) if merge else y2
-    return datas
 
 def mixing(datas, weight=None, scale=None):
     '''
