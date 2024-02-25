218e40d38251ddab1bb85505c813b1f6c2e65b17
Jianbo <zhjb1996@163.com>
2024-02-25 20:25:49 +0800

    update test case related to TEdataset

--- tests/test_TEdata.py
+++ tests/test_TEdata.py
@@ -1,43 +1,36 @@
 import unittest
 import numpy as np
 
-from teflow.loader import TEdatacol, TEdataset
+from teflow.loader import TEdataset
 
 
-class TestTEdatacol(unittest.TestCase):
-    obj = TEdatacol
+class TestTEdataset(unittest.TestCase):
     q = 1.602176634
     seq = list(range(10))
 
     def test_TRSK(self):
-        d = self.obj(data=self.seq, group='TCTSTK')
+        d = TEdataset(data=self.seq, group='TCTSTK')
         v = np.squeeze(d.gget('TRSK'))
         r = np.array([4, 10000, 3, 5])
         self.assertTrue(np.allclose(v, r))
 
     def test_KR(self):
-        d = self.obj(data=[1, 4, 250, 200, 10], group='TNUSK',)
+        d = TEdataset(data=[1, 4, 250, 200, 10], group='TNUSK',)
         v = np.squeeze(d.gget('KR'))
         r = np.array([10, 10/self.q])
         self.assertTrue(np.allclose(v, r))
 
     def test_not_KR(self):
-        d = self.obj(data=self.seq, group='TNSK')
+        d = TEdataset(data=self.seq, group='TNSK')
         with self.assertRaisesRegex(ValueError, 'Failed .* R'):
             d.gget('KR')
 
-
-class TestTEdataset(unittest.TestCase):
-    def setUp(self):
-        self.obj = TEdataset
-        self.seq = list(range(10))
-
-    def test_not_KR(self):
-        d = self.obj(data=self.seq, group='TNSK')
+    def test_not_KR_2(self):
+        d = TEdataset(data=self.seq, group='TNSK', independent=False)
         with self.assertRaisesRegex(ValueError, 'Failed .* R'):
             d.gget('KR')
 
     def test_mismatch(self):
-        d = self.obj(data=self.seq, group='TNTUSK')
+        d = TEdataset(data=self.seq, group='TNTUSK', independent=False)
         with self.assertRaisesRegex(ValueError, 'Failed .* R'):
             d.gget('KR')
