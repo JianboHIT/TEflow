c7910e05855aa19dee245f2d2fd1b36d950d93b7
Jianbo <zhjb1996@163.com>
2024-03-05 14:30:51 +0800

    provide test case of two-band model config simulating

--- tests/test_cmd/test_cmd_band/test_band.py
+++ tests/test_cmd/test_cmd_band/test_band.py
@@ -6,6 +6,7 @@ class TestCmdBand(BaseTestCmd):
     fdata = {
         'SPB': ['TS_SPB.txt', 'TS_SPB-check.txt'],
         'SKB': ['TS_SKB.txt', 'TS_SKB-check.txt'],
+        'TB': ['double_band.txt', 'double_band-check.txt'],
     }
     def test_band_TS_SPB(self):
         do_band(['-g', 'TS', '-s', 'SPB', 'TS.txt'])
@@ -13,4 +14,9 @@ class TestCmdBand(BaseTestCmd):
 
     def test_band_TS_SKB(self):
         do_band(['-g', 'TS', '--gap', '0.1', '-s', 'SKB', 'TS.txt'])
+        self.assertTrue(self.fcomp('SKB', rtol=1E-3))
+
+    def test_band_double(self):
+        # tef-band -f --T '300 323:25:873' --EF 0 double.cfg
+        do_band(['-f', '--T', '300 323:25:873', '--EF', '0', 'double.cfg'])
         self.assertTrue(self.fcomp('SKB', rtol=1E-3))
