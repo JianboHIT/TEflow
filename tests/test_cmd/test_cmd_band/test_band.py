ce9ca6c091f50de95c7ff8cdd041ae972b8313c2
Jianbo <zhjb1996@163.com>
2024-03-05 21:28:35 +0800

    provide test_band_solve() case in test_cmd_band

--- tests/test_cmd/test_cmd_band/test_band.py
+++ tests/test_cmd/test_cmd_band/test_band.py
@@ -7,6 +7,7 @@ class TestCmdBand(BaseTestCmd):
         'SPB': ['TS_SPB.txt', 'TS_SPB-check.txt'],
         'SKB': ['TS_SKB.txt', 'TS_SKB-check.txt'],
         'TB': ['double_band.txt', 'double_band-check.txt'],
+        'TBsolver': ['double_solve.txt', 'double_solve-check.txt'],
     }
     def test_band_TS_SPB(self):
         do_band(['-g', 'TS', '-s', 'SPB', 'TS.txt'])
@@ -19,4 +20,10 @@ class TestCmdBand(BaseTestCmd):
     def test_band_double(self):
         # tef-band -f --T '300 323:25:873' --EF 0 double.cfg
         do_band(['-f', '--T', '300 323:25:873', '--EF', '0', 'double.cfg'])
-        self.assertTrue(self.fcomp('SKB', rtol=1E-3))
+        self.assertTrue(self.fcomp('TB', rtol=1E-3))
+
+    def test_band_solve(self):
+        # tef-band -f --T '300 323:25:873' --EF '100 @ N' --initial -10 -s solve  double.cfg
+        do_band(['-f', '--T', '300 323:25:873', '--EF', '100 @ N',
+                 '--initial', '-10', '-s', 'solve', 'double.cfg'])
+        self.assertTrue(self.fcomp('TBsolver', rtol=1E-3))
