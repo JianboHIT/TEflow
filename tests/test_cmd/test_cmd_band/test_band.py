34f51afbe661121f818724993d137087cf4d418b
Jianbo <zhjb1996@163.com>
2024-03-10 14:43:15 +0800

    provide test case of RSPB modelling band

--- tests/test_cmd/test_cmd_band/test_band.py
+++ tests/test_cmd/test_cmd_band/test_band.py
@@ -5,6 +5,7 @@ class TestCmdBand(BaseTestCmd):
     fwork = __file__
     fdata = {
         'SPB': ['TS_SPB.txt', 'TS_SPB-check.txt'],
+        'RSPB': ['TS_RSPB.txt', 'TS_SPB-check.txt'],    # compare RSPB with SPB
         'SKB': ['TS_SKB.txt', 'TS_SKB-check.txt'],
         'TB': ['double_band.txt', 'double_band-check.txt'],
         'TBsolver': ['double_solve.txt', 'double_solve-check.txt'],
@@ -12,6 +13,11 @@ class TestCmdBand(BaseTestCmd):
     def test_band_TS_SPB(self):
         do_band(['-g', 'TS', '-m', 'SPB', '-s', 'SPB', 'TS.txt'])
         self.assertTrue(self.fcomp('SPB', rtol=1E-3))
+
+    def test_band_TS_RSPB(self):
+        do_band(['-g', 'TS', '-m', 'RSPB', '-s', 'RSPB', 'TS.txt'])
+        self.assertFalse(self.fcomp('RSPB', atol=0.00001))  # not exact equal
+        self.assertTrue(self.fcomp('RSPB', atol=0.03))  # but approximately equal
 
     def test_band_TS_SKB(self):
         do_band(['-g', 'TS', '-m', 'SKB', '--gap', '0.1', '-s', 'SKB', 'TS.txt'])
