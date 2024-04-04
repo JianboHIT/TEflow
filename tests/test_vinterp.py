709d56c56ae70f3735107884fb64f2e47a3b880d
Jianbo <zhjb1996@163.com>
2024-04-04 14:15:19 +0800

    provide TestInterpGPR() in tests/test_vinterp.py

--- tests/test_vinterp.py
+++ tests/test_vinterp.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from teflow.mathext import vinterp
+from teflow.mathext import vinterp, Metric
 
 
 class TestVinterp(unittest.TestCase):
@@ -40,5 +40,31 @@ class TestVinterp(unittest.TestCase):
         )
         yref = [-404, 2, 4, 6, 8, 10, 12, 14, 16, 16, 16]
         self.assertTrue(np.allclose(yp, yref))
-        
-        
+
+
+class TestInterpGPR(unittest.TestCase):
+    def model(self, x):
+        return x * np.cos(x)
+
+    def gradient(self, x):
+        return np.cos(x) - x*np.sin(x)
+
+    def test_interp_gpr(self):
+        xp = np.linspace(0, 12, 121)
+        z_real = self.model(xp)
+        dz_real = self.gradient(xp)
+
+        xi = np.arange(1, 12, 2)
+        zi = self.model(xi)
+
+        paras = dict(kernel='rbf', method='gpr', scale=2.5, regular=1E-3)
+        zp = vinterp(xi, zi, xp, **paras)
+        dzp = vinterp(xi, zi, xp, gradient=True, **paras)
+
+        rmse_zp = Metric.RMSE(zp, z_real)
+        rmse_dzp = Metric.RMSE(dzp, dz_real)
+        # np.savetxt('data_gpr.txt', np.c_[xp, zp, z_real, dzp, dz_real],
+        #            fmt='%.6f', header='zp z_real dzp dz_real')
+        self.assertLess(rmse_zp, 0.2)
+        self.assertLess(rmse_dzp, 0.4)
+
