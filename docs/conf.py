c5e35d7b6af1f85a5ea080833497e2891f05bc68
JianboHIT <zhjb1996@163.com>
2025-05-15 15:53:54 +0800

    update conf.py for supporting of matplotlib plot

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.githubpages',
+    'matplotlib.sphinxext.plot_directive',
 ]
 
 templates_path = ['_templates']
@@ -30,7 +31,7 @@ language = 'zh_CN'
 locale_dirs = ['locale/']
 gettext_compact = False
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML and Latex output ----------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # alabaster, classic, sphinxdoc, scrolls, agogo, traditional, nature, haiku, pyramid, bizstyle
@@ -46,4 +47,56 @@ latex_elements = {
        \setlength{\parindent}{2em}
     ''',
     'printindex': r'\def\twocolumn[#1]{#1}\printindex',
+}
+
+# -- Options for Matplotlib plot ---------------------------------------------
+plot_formats = [('png', 300), ('hires.png', 600), ('pdf', 300)]
+plot_html_show_formats = False
+plot_html_show_source_link = False
+plot_rcparams = {
+    # Figure settings
+    'figure.figsize': (4, 3),
+
+    # Subplot parameters
+    'figure.subplot.left': 0.15,
+    'figure.subplot.right': 0.85,
+    'figure.subplot.bottom': 0.15,
+    'figure.subplot.top': 0.90,
+    'figure.subplot.wspace': 0.4,
+    'figure.subplot.hspace': 0.4,
+
+    # Font settings
+    'font.family': 'Arial',
+    'font.size': 9,
+
+    # Math text settings
+    'mathtext.fontset': 'cm',
+
+    # Tick settings
+    'xtick.direction': 'in',
+    'xtick.labelsize': 'medium',
+    'xtick.major.width': 1,
+    'xtick.major.size': 4,
+    'xtick.minor.width': 1,
+    'xtick.minor.size': 2.5,
+    'xtick.minor.visible': True,
+    'xtick.minor.ndivs': 2,
+
+    'ytick.direction': 'in',
+    'ytick.labelsize': 'medium',
+    'ytick.major.width': 1,
+    'ytick.major.size': 4,
+    'ytick.minor.width': 1,
+    'ytick.minor.size': 2.5,
+    'ytick.minor.visible': True,
+    'ytick.minor.ndivs': 2,
+
+    # Axes settings
+    'axes.linewidth': 1.5,
+    'axes.labelsize': 'large',
+
+    # Legend settings
+    'legend.fancybox': False,
+    'legend.fontsize': 'medium',
+    'legend.borderpad': 0.2,
 }
